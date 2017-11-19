0174a1b191fac7b82e0f2993a45d6608a11e104a
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-11-19 18:06:50 +0100

    added InvoiceReport class and unit test

---
+++ src/Invoice/InvoiceReport.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexpedia\Invoice;
+
+use Flexpedia\CsvOutput;
+use Flexpedia\Invoice\Entity\InvoiceInterface;
+use Flexpedia\Invoice\Repository\InvoiceRepositoryInterface;
+
+/**
+ * Description of InvoiceReport
+ * 
+ * @author Balázs Máté Petró <petrobalazsmate@gmail.com>
+ */
+final class InvoiceReport
+{
+    /**
+     * The invoice repository.
+     *
+     * @var InvoiceRepositoryInterface
+     */
+    private $invoice;
+
+    /**
+     * The CSV renderer.
+     *
+     * @var CsvOutput
+     */
+    private $csvOutput;
+    
+    /**
+     * Creates a new InvoiceList instance.
+     *
+     * @param InvoiceRepositoryInterface $invoice The invoice repository instance.
+     */
+    public function __construct(InvoiceRepositoryInterface $invoice, CsvOutput $csvOutput)
+    {
+        $this->invoice = $invoice;
+        $this->csvOutput = $csvOutput;
+    }
+
+    /**
+     * Creates and returns the CSV output of invoices.
+     *
+     * @return string
+     */
+    public function __invoke()
+    {
+        $data = array_map(function ($invoice) {
+            return $this->mapInvoice($invoice);
+        }, $this->invoice->findAll());
+
+        return $this->csvOutput->render($data);
+    }
+
+    /**
+     * Maps and returns the given Invoice instance to an array.
+     *
+     * @param InvoiceInterface $invoice The Invoice instance.
+     * @return array
+     */
+    private function mapInvoice(InvoiceInterface $invoice)
+    {
+        return [
+            $invoice->getId(),
+            $invoice->getClient(),
+            $invoice->getAmount()
+        ];
+    }
+}
---
+++ tests/Invoice/InvoiceReportTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Flexpedia\Test\Invoice;
+
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+use Flexpedia\CsvOutput;
+use Flexpedia\Invoice\InvoiceReport;
+use Flexpedia\Invoice\Entity\Invoice;
+use Flexpedia\Invoice\Repository\InvoiceRepositoryInterface;
+use Flexpedia\Invoice\Status\Paid;
+
+/**
+ * Description of InvoiceReportTest
+ * 
+ * @author Balázs Máté Petró <petrobalazsmate@gmail.com>
+ */
+class InvoiceReportTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $repository = Mockery::mock(InvoiceRepositoryInterface::class);
+        $repository->shouldReceive('findAll')->once()->andReturn([
+            new Invoice(
+                1,
+                'Client 1',
+                1.0,
+                1.2,
+                0.2,
+                new Paid,
+                DateTime::createFromFormat('Y-m-d', '2017-11-19'),
+                DateTime::createFromFormat('Y-m-d H:i:s', '2017-11-19 10:10:10'),
+                []
+            ),
+            new Invoice(
+                2,
+                'Client 2',
+                2.0,
+                2.2,
+                0.2,
+                new Paid,
+                DateTime::createFromFormat('Y-m-d', '2017-11-19'),
+                DateTime::createFromFormat('Y-m-d H:i:s', '2017-11-19 11:11:11'),
+                []
+            )
+        ]);
+
+        $output = (new InvoiceReport($repository, new CsvOutput))->__invoke();
+
+        $lines = explode("\n", $output);
+
+        $this->assertEquals('1,"Client 1",1', $lines[0]);
+        $this->assertEquals('2,"Client 2",2', $lines[1]);
+
+        Mockery::close();
+    }
+}
